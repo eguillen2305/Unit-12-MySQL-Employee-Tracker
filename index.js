bb620f338c1ae60507f1a4b0785168cc5d87e11f
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-08 12:35:35 -0500

    added async function

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const { prompt } = require("inquirer");
 const { inherits } = require("util");
+const db = require("./db");
 
 
 
 init();
+
+async function starterQuestions() {
+    const { choice } = awai prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees"
+                    value: "VIEW_EMPLOYEES"
+               }, 
+            ]
+        }
+    ])
+}
