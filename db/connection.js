4d4f54e7dd6fc7ea271e1f47d6962edc5e8d3271
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-11 15:51:00 -0500

    Updated connection

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,17 @@
-const util = require("util");
-const mysql = require("mysql");
+const util = require('util');
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "employeeTracker_DB"
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'employeeTracker_DB'
 });
 
-connection.connect();
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log('connected as id ' + connection.threadId);
+});
 
 connection.query = util.promisify(connection.query);
 
