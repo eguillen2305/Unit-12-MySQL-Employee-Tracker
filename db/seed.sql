6867d45f227446fe28057ebdb078acf07e952e63
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-11 13:01:47 -0500

    Inserted sample seed values for INSERT INTO EMPLOYEEs

--- db/seed.sql
+++ db/seed.sql
@@ -15,5 +15,14 @@ VALUES
     ('Accounts Payable', 500000, 1),
     ('Lawyer', 120000, 2),
     ('Account Manager', 110000, 3),
-    ('Helpdesk', 80000, 4),
-    
+    ('Helpdesk', 80000, 4)
+,
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Tim', 'Johnson', 1, 2),
+    ('Sandra', 'Enciso', 2, 1),
+    ('Steven', 'Rodriguez', 3, NULL),
+    ('Bravo', 'Kao', 4, 3)
+,
