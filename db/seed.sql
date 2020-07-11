4e4b20cfd72d1418bddf10d5197d7797678ca1e6
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-11 12:58:37 -0500

    Added sample values

--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,19 @@
 use employeeTracker_DB;
 
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Accounting'),
+    ('Legal'),
+    ('Sales'),
+    ('IT');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Accounts Payable', 500000, 1),
+    ('Lawyer', 120000, 2),
+    ('Account Manager', 110000, 3),
+    ('Helpdesk', 80000, 4),
+    
