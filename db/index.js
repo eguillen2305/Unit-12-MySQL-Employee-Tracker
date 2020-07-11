6e8278d654a0c06b968535d1336326b3c2367b13
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-11 16:21:33 -0500

    create, remove, and department

--- db/index.js
+++ db/index.js
@@ -1,6 +1,25 @@
-const connection = require("./connection");
+const connection = require('./connection');
 
 class DB {
-  constructor(connection) {
-    this.connection = connection;
-  }
+	constructor(connection) {
+		this.connection = connection;
+	}
+
+	createRole(role) {
+		return this.connection.query('INSERT INTO role SET ?', role);
+	}
+
+	removeRole(roleId) {
+		return this.connection.query('DELETE FROM role WHERE id = ?', roleId);
+	}
+
+	createDepartment(department) {
+		return this.connection.query('INSERT INTO department SET ?', department);
+	}
+
+	removeDepartment(departmentId) {
+		return this.connection.query('DELETE FROM department WHERE id = ?', departmentId);
+	}
+}
+
+module.exports = new DB(connection);
